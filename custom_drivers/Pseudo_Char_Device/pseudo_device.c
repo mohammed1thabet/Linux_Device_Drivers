a47ea734614200ed66a9209344c270fb9074bdc6
Mohammed Thabet <mohammed.1.alagamy@gmail.com>
2024-10-04 14:54:08 +0300

    implement file operations

--- custom_drivers/Pseudo_Char_Device/pseudo_device.c
+++ custom_drivers/Pseudo_Char_Device/pseudo_device.c
@@ -18,8 +18,23 @@ dev_t dev_num;
 /*cdev var*/
 struct cdev pseudo_cdev;
 
+/*file operations*/
+loff_t pseudo_llseek (struct file *filePtr, loff_t offset, int whence);
+ssize_t pseudo_read (struct file *filePtr, char __user *buffer, size_t count, loff_t *f_pos);
+ssize_t pseudo_write (struct file *filePtr, const char __user *buffer, size_t count, loff_t *f_pos);\
+int pseudo_open (struct inode *inodePtr, struct file *filePtr);
+int pseudo_release (struct inode *inodePtr, struct file *filePtr);
+
 /*file_operations struct*/
-struct file_operations pseudo_fops;
+struct file_operations pseudo_fops = {
+    .open       = pseudo_open,
+    .release    = pseudo_release,
+    .read       = pseudo_read,
+    .write      = pseudo_write,
+    .llseek     = pseudo_llseek,
+    .owner      = THIS_MODULE
+};
+
 struct class *pseudo_class;
 
 struct device *pseudo_device;
@@ -106,6 +121,104 @@ static void __exit pseudo_deinit(void)
 
     unregister_chrdev_region(dev_num, 1);
 
+}
+
+int pseudo_open (struct inode *inodePtr, struct file *filePtr)
+{
+    pr_info("pseudo_open method called:\n");
+	return 0;
+}
+int pseudo_release (struct inode *inodePtr, struct file *filePtr)
+{
+    pr_info("pseudo_release method called:\n");
+	return 0;
+}
+
+ssize_t pseudo_read (struct file *filePtr, char __user *buffer, size_t count, loff_t *f_pos)
+{
+    pr_info("pseudo_read method called, count:%zu, file position:%lld\n", count, *f_pos);
+
+    /*if the count exeeds the memory size truncate the count*/
+    if((count + *f_pos) > DEV_MEM_SIZE)
+        count = DEV_MEM_SIZE - *f_pos;
+    
+    /*copy data, note that this code is not thread safe*/
+    if(copy_to_user(buffer, &device_mem[*f_pos], count) > 0)
+        return -EFAULT;
+
+    /*update file position*/
+    *f_pos = *f_pos + count;
+
+    pr_info("number of bytes have been read%zu, file position:%lld\n", count, *f_pos);
+	return count;
+}
+
+ssize_t pseudo_write (struct file *filePtr, const char __user *buffer, size_t count, loff_t *f_pos)
+{
+	pr_info("pseudo_write method called, count:%zu, file position:%lld\n", count, *f_pos);
+
+    if(*f_pos == DEV_MEM_SIZE)
+    {
+        /*EOF reached*/
+        pr_info("no space left\n");
+        return -ENOMEM;
+    }
+    
+    /*if the count exeeds the memory size truncate the count*/
+    if((count + *f_pos) > DEV_MEM_SIZE)
+        count = DEV_MEM_SIZE - *f_pos;
+    
+    /*copy data, note that this code is not thread safe*/
+    if(copy_from_user(&device_mem[*f_pos], buffer, count) > 0)
+        return -EFAULT;
+
+    /*update file position*/
+    *f_pos = *f_pos + count;
+        
+    pr_info("number of bytes have been written%zu, file position:%lld\n", count, *f_pos);
+	return count;
+}
+
+loff_t pseudo_llseek (struct file *filePtr, loff_t offset, int whence)
+{
+    pr_info("pseudo_llseek method called, current f_pos:%lld\n", filePtr->f_pos);
+    loff_t newPos;
+    switch (whence)
+    {
+        case SEEK_SET:
+            /*return error if the file position will go beyond file memory or if it will be <0*/
+            if((offset > DEV_MEM_SIZE) || (offset < 0))
+                return -EINVAL;
+
+            filePtr->f_pos = offset;
+        break;
+
+        case SEEK_CUR:
+            newPos = filePtr->f_pos +offset;
+            
+            /*return error if the file position will go beyond file memory or if it will be <0*/
+            if( (newPos > DEV_MEM_SIZE) || (newPos < 0) )
+                return -EINVAL;
+
+            filePtr->f_pos = newPos;
+        break;
+        
+        case SEEK_END:
+            newPos = DEV_MEM_SIZE + offset;
+
+            /*return error if the file position will go beyond file memory or if it will be <0*/
+            if( (newPos > DEV_MEM_SIZE) || (newPos < 0) )
+                return -EINVAL;
+
+            filePtr->f_pos = newPos;
+        break;
+        
+        default:
+            return -EINVAL;
+        break;
+    }
+    pr_info("new f_pos:%lld\n", filePtr->f_pos);
+	return filePtr->f_pos;
 }
 
 /*registration section*/
